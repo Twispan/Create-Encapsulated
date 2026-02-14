edb3d58537ca20327662a3f59909d87d9a83644a
Twispan <dannysofi80@gmail.com>
2026-02-14 01:16:23 -0500

    re-organized mixing folder

--- src/main/java/com/twispan/create_encapsulated/fluid/paint/PaintColor.java
+++ src/main/java/com/twispan/create_encapsulated/fluid/paint/PaintColor.java
@@ -3,19 +3,19 @@ package com.twispan.create_encapsulated.fluid.paint;
 public enum PaintColor {
     RED(0xFF5A3A),
     BLUE(0x3789FD),
-    GREEN(0x60C10D),
-    YELLOW(0xF8D332),
+    GREEN(0x0FB009),
+    YELLOW(0xFDDE1A),
     PINK(0xE89DC9),
-    PURPLE(0xA35DCC),
+    PURPLE(0x9B5DCC),
     MAGENTA(0xD673D0),
     LIME(0x84F725),
     CYAN(0x4CBCC0),
-    LIGHTBLUE(0x80B4FF),
+    LIGHTBLUE(0x72BEFF),
     ORANGE(0xFEAC3A),
     BROWN(0xB9754E),
-    LIGHTGRAY(0xB0B0B0),
-    GRAY(0x898989),
-    BLACK(0x656C8B),
+    LIGHTGRAY(0xAAAAB0),
+    GRAY(0x808086),
+    BLACK(0x50597C),
     WHITE(-1);
 
     private final int color;
