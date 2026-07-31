cdf721ada077899ddf77f41a6fa1545c1c74cc93
Fire <38797905+itsfirestorm@users.noreply.github.com>
2026-07-31 18:03:43 +0200

    Forgot to purge this

--- src/main/java/com/twispan/create_encapsulated/client/ModClientSetup.java
+++ src/main/java/com/twispan/create_encapsulated/client/ModClientSetup.java
@@ -3,7 +3,6 @@ package com.twispan.create_encapsulated.client;
 import com.twispan.create_encapsulated.CreateEncapsulated;
 import com.twispan.create_encapsulated.fluid.MedicinalBrewFluidType;
 import com.twispan.create_encapsulated.fluid.other_medicine.OMedicineFluidType;
-import com.twispan.create_encapsulated.fluid.paint.PaintFluidType;
 import com.twispan.create_encapsulated.fluid.potions.PotionFluidType;
 import com.twispan.create_encapsulated.fluid.vitamins.VitaminFluidType;
 import com.twispan.create_encapsulated.registries.ModFluids;
@@ -21,23 +20,6 @@ public class ModClientSetup {
         ResourceLocation LIQUID_MEDIUM_FLOW = ResourceLocation.fromNamespaceAndPath(CreateEncapsulated.MODID, "block/liquid_medium_flow");
         ResourceLocation LIQUID_STRONG_STILL = ResourceLocation.fromNamespaceAndPath(CreateEncapsulated.MODID, "block/liquid_strong_still");
         ResourceLocation LIQUID_STRONG_FLOW = ResourceLocation.fromNamespaceAndPath(CreateEncapsulated.MODID, "block/liquid_strong_flow");
-
-        registerPaintFluidRendering(event, ModFluids.RED_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.BLUE_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.GREEN_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.YELLOW_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.PINK_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.BLACK_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.WHITE_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.PURPLE_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.MAGENTA_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.LIME_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.CYAN_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.LIGHTBLUE_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.ORANGE_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.BROWN_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.LIGHTGRAY_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
-        registerPaintFluidRendering(event, ModFluids.GRAY_PAINT_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
 
         registerMedicinalBrewRendering(event, ModFluids.MEDICINAL_BREW_TYPE.get(), LIQUID_SOFT_STILL, LIQUID_SOFT_FLOW);
 
@@ -66,37 +48,6 @@ public class ModClientSetup {
         registerOMedicineFluidRendering(event, ModFluids.MAX_ETHER_TYPE.get(), LIQUID_MEDIUM_STILL, LIQUID_MEDIUM_FLOW);
         registerOMedicineFluidRendering(event, ModFluids.ELIXIR_TYPE.get(), LIQUID_MEDIUM_STILL, LIQUID_MEDIUM_FLOW);
         registerOMedicineFluidRendering(event, ModFluids.MAX_ELIXIR_TYPE.get(), LIQUID_MEDIUM_STILL, LIQUID_MEDIUM_FLOW);
-    }
-
-    private static void registerPaintFluidRendering(
-            RegisterClientExtensionsEvent event,
-            PaintFluidType fluidType,
-            ResourceLocation still,
-            ResourceLocation flow
-    ) {
-        event.registerFluidType(new IClientFluidTypeExtensions() {
-            @Override
-            public @NotNull ResourceLocation getStillTexture() {
-                return still;
-            }
-
-            @Override
-            public ResourceLocation getOverlayTexture() {
-                return still;
-            }
-
-            @Override
-            public @NotNull ResourceLocation getFlowingTexture() { return flow; }
-
-            @Override
-            /* ARGB Format: 0xAARRGGBB
-            * AA: Alpha || RR: Red || GG: Green || BB: Blue
-            * We want paint to be fully opaque since it is a dense liquid.
-            */
-            public int getTintColor() {
-                return 0xFF000000 | fluidType.getPaintColor().getColor();
-            }
-        }, fluidType);
     }
 
     private static void registerMedicinalBrewRendering(
